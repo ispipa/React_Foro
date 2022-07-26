fe6e02964bd7800c98c5259bdd4c29b53c300e1d
Fernanda <mariferrm97@hotmail.com>
2022-07-26 13:57:48 +0200

    login

---
+++ src/components/Buttons/MainButtons.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainButtons = ({mainbutton}) => {
+    return (
+
+        <button>{mainbutton}</button>
+        
+    );
+};
+
+export default MainButtons;
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import MainButtons from '../Buttons/MainButtons';
 
 const Login = () => {
+  const[email, setEmail] = useState("")
 
-    return (
-      <div>
-         
-      </div>
-    );
+  console.log(email);
+  const data = (e) => {
+  e.preventDefault()
+  
+}
+
+  return (
+    <div>
+      <form onSubmit={data}>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">Email</label>
+          <input type="email" className="form-control" name="" id="email" onChange={e => setEmail(e.target.value)} aria-describedby="helpId" placeholder=""/>
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">Contraseña</label>
+          <input type="password" className="form-control" name="" id="password" aria-describedby="helpId" placeholder=""/>
+        </div>
+
+        <MainButtons mainbutton={"Enviar"}/>
+      
+      </form>
+    </div>
+);
+
 };
 
-export default Login;
+        export default Login;
