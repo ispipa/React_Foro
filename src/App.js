c9868c01ecca4707e624f2b38e7bd4afba535611
rgildeprado <rgildeprado@gmail.com>
2022-07-26 20:45:44 +0200

    iniciamos registro

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Login from './components/Login/Login';
+import Register from './components/Login/Register';
 
 function App() {
   return (
     <>
     <Login />
-    
+    <Register/>
     </>
   );
 }
--- src/components/Login/Register.js
+++ src/components/Login/Register.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import MainButtons from '../Buttons/MainButtons';
 
 const Register = () => {
     return (
         <div>
-            Este es el registro
+            <form>
+                <div>
+                    <label htmlfor>Username</label>
+                    <input type="text" id='username' name='username'/>
+                </div>
+
+                <div>
+                    <label htmlfor>Email</label>
+                    <input type="email" id='email' name='email'/>
+                </div>
+
+                <div>
+                    <label htmlfor>Contraseña</label>
+                    <input type="password" id='password' name='password'/>
+                </div>
+
+                <div>
+                    <label htmlfor>Confirma contraseña</label>
+                    <input type="password" id='password' name='password'/>
+                </div>
+                <MainButtons mainbutton='Regístrate'/>
+            </form> 
         </div>
     );
 };
