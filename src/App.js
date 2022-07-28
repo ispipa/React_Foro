4dd3ff936d25769b94b8621479c6f636114ff82a
Fernanda <mariferrm97@hotmail.com>
2022-07-28 09:06:33 +0200

    cambios html

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
+import Home from './components/Index/Home';
 import Login from './components/Login/Login';
 import Register from './components/Login/Register';
 
 function App() {
   return (
     <>
-    <Login />
-    <Register/>
+    {/* <Login />
+    <Register/> */}
+    <Home/>
     </>
   );
 }
---
+++ src/components/Index/Home.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const home = () => {
+    return (
+        <div>
+            <section>
+                <h1>Encuentra un tema del que hablar</h1>
+            </section>
+            <section>
+                <div>
+                    <img></img>
+                    <h2>Empleo/Emprendimiento</h2>
+                </div>
+                <div>
+                    <img></img>
+                    <h2>Finanzas</h2>
+                </div>
+                <div>
+                    <img></img>
+                    <h2>Salud y entrenamiento</h2>
+                </div>
+                <div>
+                    <img></img>
+                    <h2>Relaciones</h2>
+                </div>
+                <div>
+                    <img></img>
+                    <h2>Viajes</h2>
+                </div>
+                <div>
+                    <img></img>
+                <h2>Ocio y videojuegos</h2>
+                </div> 
+            </section>
+        </div>
+    );
+};
+
+export default home;
