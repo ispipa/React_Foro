e1c3ead8a7f911d719e2df5133a88c847f5f4331
ispipa <ispipa123@gmail.com>
2022-07-29 17:19:05 +0200

    crear tema

--- server/clases/mensajes_hilos.class.php
+++ server/clases/mensajes_hilos.class.php
@@ -12,7 +12,7 @@ class mensajes_hilos extends conexion
     //obtenemos los mensajes de un hilo a traves de su id
     public function obtenerMensajes($id)
     {
-        $query = "SELECT nombre,email,texto_mensaje FROM mensajes join hilos on mensajes.id_hilo = hilos.id_usuario join usuarios on mensajes.id_usuario = usuarios.id where hilos.id = '$id'";
+        $query = "SELECT nombre,email,texto_mensaje FROM mensajes join hilos on mensajes.id_hilo = hilos.id join usuarios on mensajes.id_usuario = usuarios.id where hilos.id = '$id'";
         return parent::obtenerDatos($query);
     }
 }
--- server/clases/temas.class.php
+++ server/clases/temas.class.php
@@ -22,5 +22,49 @@ class temas extends conexion
         return $datos;
     }
 
+    public function post($json)
+    {
+        $datos = json_decode($json,true);
+        print_r($datos);
+        if(isset($datos['tema']) && isset($datos['img']['tmp_name']))
+        {
+            $this->temas_name = $datos['tema'];
+            $this->img = $datos['img']['tmp_name'];
+            $imgContent = addslashes(file_get_contents( $this->img));
+            print_r($this->temas_name);
+            echo "\n";
+            print_r($this->img);
+            echo "\n";
+            $resp = $this->isertTema();
+            if($resp)
+            {
+                //$respuesta = $respuesta->response;
+                $respuesta['result'] = array("usuarioId" =>$resp);
+                return json_encode($respuesta);
+            }
+            else
+            {
+                http_response_code(403);
+                return json_encode(array("mesage" => "Todo mal"));
+            }
+        }
+    }
+
+    //alta a un tema con img
+    private  function isertTema()
+    {
+        
+        $query ="INSERT INTO ".$this->table . "(temas,img) values('" . $this->temas_name . "','" . $this->img . "')";
+        $resp = parent::nonQueryId($query);
+        if($resp)
+        {
+            return $resp;
+        }
+        else
+        {
+           return 0;
+        }
+    }
+
 }
 ?>
